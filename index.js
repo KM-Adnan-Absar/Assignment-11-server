fb348239bc56d60103ebf7f227083c300178c615
KM Adnan Absar <mdadnanjilan1234@gmail.com>
2025-02-11 00:57:10 +0600

    fourth-commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
+
     deprecationErrors: true,
   }
 });
@@ -37,14 +38,32 @@ async function run() {
 }
 run().catch(console.dir);
 
+const submitAssignmentCollection = client.db('Online-Assignment').collection('Assignment')
 const createAssignmentCollection = client.db('Online-Assignment').collection('create_assignment')
 
+app.get('/submitAssignment',async (req , res)=> {
+  const cursor =  submitAssignmentCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
 app.post('/create-assignment', async (req,res) => {
 
     const application = req.body;
  const result = await createAssignmentCollection.insertOne(application)
 res.send(result)
 })
+
+app.put("/updateAssignment/:id", async (req, res) => {
+  const { id } = req.params;
+  const { obtainedMarks, feedback, status } = req.body;
+
+  try {
+    await Assignment.findByIdAndUpdate(id, { obtainedMarks, feedback, status });
+    res.json({ message: "Assignment updated successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: "Error updating assignment" });
+  }
+});
 
 
 app.listen(port, () => {
